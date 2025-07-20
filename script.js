fb9842c37c14006a5e34a1a7855415f753440c70
Nel1304 <nh97189@gmail.com>
2025-07-19 21:24:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const musica = document.getElementById('musicaFondo');
+
+function activarTransiciones() {
+  const slides = document.querySelectorAll('.slide');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+  slides.forEach(slide => observer.observe(slide));
+}
+
+function abrirFormulario() {
+  document.getElementById('modalConfirmacion').style.display = 'flex';
+}
+
+function cerrarFormulario() {
+  document.getElementById('modalConfirmacion').style.display = 'none';
+  document.getElementById('formConfirmacion').reset();
+  document.getElementById('nombreAcompanante').style.display = 'none';
+  document.getElementById('mensajeGracias').style.display = 'none';
+  document.getElementById('formConfirmacion').style.display = 'block';
+}
+
+function mostrarCampoAcompanante() {
+  const select = document.getElementById('conAcompanante');
+  const campo = document.getElementById('nombreAcompanante');
+  campo.style.display = (select.value === 'si') ? 'block' : 'none';
+}
+
+function enviarConfirmacion(e) {
+  e.preventDefault();
+  const nombre = document.getElementById('nombreInvitado').value.trim();
+  const mensaje = document.getElementById('mensajeGracias');
+  mensaje.innerText = `¡Gracias, ${nombre}!, estamos felices por nuestra celebración, agradecemos tu compañía.`;
+
+  document.getElementById('formConfirmacion').style.display = 'none';
+  mensaje.style.display = 'block';
+
+  setTimeout(() => {
+    cerrarFormulario();
+    document.getElementById('el-gran-dia').scrollIntoView({
+      behavior: 'smooth'
+    });
+  }, 2000);
+}
+
+// Código para abrir invitación al hacer click en el sello de cera
+const sello = document.getElementById('sello');
+const wrapper = document.getElementById('wrapper');
+
+sello.addEventListener('click', () => {
+  wrapper.classList.add('open');
+
+  setTimeout(() => {
+    const content = document.getElementById('invitacion');
+    content.classList.add('fullscreen');
+    activarTransiciones();
+
+    musica.play().catch(error => {
+      console.warn("Reproducción automática bloqueada:", error);
+    });
+  }, 1200);
+});
